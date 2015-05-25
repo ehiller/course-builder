c3030c9102c2b4532be894b1af549be376ee9933
ehiller <ehiller@css.edu>
2015-05-25 12:11:40 -0500

    Added teacher_dashboard directory. Setup basic handler and registration, including tabs. Added module to app.yaml to kick off registration

---
+++ coursebuilder/modules/teacher_dashboard/teacher_dashboard.py
@@ -0,0 +1,242 @@
+__author__ = 'ehiller@css.edu'
+
+
+# Module to support custom teacher views in CourseBuilder dashboard
+# Views include:
+#       Class Roster - paginated list of students in teacher's section
+#       CourseAdmin/Create Section - page for course admins to add a section for a teacher.
+#           This will create a teacher entity
+#       Student Dashboard - view of a single student's performance in the course
+#       Section Gradebook - show course completion
+#       More to be added
+
+import logging
+import jinja2
+import os
+
+import appengine_config
+
+from google.appengine.ext import db
+
+from common import tags
+from common import crypto
+
+from models import courses
+from models import custom_modules
+from models import models
+from models import roles
+
+#since we are extending the dashboard, probably want to dashboard stuff
+from modules.dashboard import dashboard
+from modules.dashboard import tabs
+
+#Setup paths and directories for templates and resources
+RESOURCES_PATH = '/modules/teacher_dashboard/resources'
+
+TEMPLATES_DIR = os.path.join(
+    appengine_config.BUNDLE_ROOT, 'modules', 'teacher_dashboard', 'templates')
+
+#setup permissions that will be registered with the dashboard
+ACCESS_ASSETS_PERMISSION = 'can_access_assets'
+ACCESS_ASSETS_PERMISSION_DESCRIPTION = 'Can access the Assets Dashboard'
+
+ACCESS_SETTINGS_PERMISSION = 'can_access_settings'
+ACCESS_SETTINGS_PERMISSION_DESCRIPTION = 'Can access the Settings Dashboard'
+
+ACCESS_ROLES_PERMISSION = 'can_access_roles'
+ACCESS_ROLES_PERMISSION_DESCRIPTION = 'Can access the Roles Dashboard'
+
+ACCESS_ANALYTICS_PERMISSION = 'can_access_analytics'
+ACCESS_ANALYTICS_PERMISSION_DESCRIPTION = 'Can access the Analytics Dashboard'
+
+ACCESS_SEARCH_PERMISSION = 'can_access_search'
+ACCESS_SEARCH_PERMISSION_DESCRIPTION = 'Can access the Search Dashboard'
+
+ACCESS_PEERREVIEW_PERMISSION = 'can_access_peer_review'
+ACCESS_PEERREVIEW_PERMISSION_DESCRIPTION = 'Can access the Peer Review Dashboard'
+
+ACCESS_SKILLMAP_PERMISSION = 'can_access_skill_map'
+ACCESS_SKILLMAP_PERMISSION_DESCRIPTION = 'Can access the Skill Map Dashboard'
+
+#setup custom module for, needs to be referenced later
+custom_module = None
+
+class BaseDashboardExtension(object):
+    ACTION = None
+
+    @classmethod
+    def is_readonly(cls, course):
+        return course.app_context.get_environ()[
+                'course'].get('prevent_translation_edits')
+
+    @classmethod
+    def format_readonly_message(cls):
+        pass
+        #return safe_dom.Element('P').add_text(
+        #    'Translation console is currently disabled. '
+        #    'Course administrator can enable it via I18N Settings.')
+
+    @classmethod
+    def register(cls):
+        def get_action(handler):
+            cls(handler).render()
+        dashboard.DashboardHandler.add_custom_get_action(cls.ACTION, get_action)
+        #dashboard.DashboardHandler.map_action_to_permission(
+        #    'get_%s' % cls.ACTION, ACCESS_PERMISSION)
+
+    @classmethod
+    def unregister(cls):
+        dashboard.DashboardHandler.remove_custom_get_action(cls.ACTION)
+        dashboard.DashboardHandler.unmap_action_to_permission(
+            'get_%s' % cls.ACTION)
+
+    def __init__(self, handler):
+        """Initialize the class with a request handler.
+
+        Args:
+            handler: modules.dashboard.DashboardHandler. This is the handler
+                which will do the rendering.
+        """
+        self.handler = handler
+
+class TeacherHandler(BaseDashboardExtension, dashboard.DashboardHandler):
+    ACTION = 'teacher_dashboard'
+    DEFAULT_TAB = 'sections'
+
+    XSRF_TOKEN_NAME = ''
+
+    def __init__(self, handler):
+        super(TeacherHandler, self).__init__(handler)
+
+    @classmethod
+    def register_tabs(cls):
+
+        def register_tab(key, label, handler, href=None):
+            if href:
+                target = '_blank'
+            else:
+                href = 'dashboard?action=teacher_dashboard&tab=%s' % key
+                target = None
+
+            tabs.Registry.register(
+                cls.ACTION, key, label, contents=handler, href=href, target=target
+            )
+
+        register_tab('sections', 'Sections', TeacherHandler)
+        register_tab('student', 'Student Profile', TeacherHandler)
+
+        # skill_map_visualization = analytics.Visualization(
+        #     'skill_map',
+        #     'Skill Map Analytics',
+        #     'templates/skill_map_analytics.html',
+        #     data_source_classes=[SkillMapDataSource])
+        # tabs.Registry.register('analytics', 'skill_map', 'Skill Map',
+        #                        [skill_map_visualization])
+
+    def render(self, template_values, in_action=None, in_tab=None):
+        pass
+        #main_content = self.get_template(
+        #    'teacher_dashboard.html', [TEMPLATES_DIR]).render(template_values)
+
+        #self.render_page({
+        #    'page_title': self.format_title('Teacher Dashboard'),
+        #    'main_content': jinja2.utils.Markup(main_content),
+        #    })
+
+    @classmethod
+    def display_html(cls, handler):
+        in_tab = handler.request.get('tab') or cls.DEFAULT_TAB
+
+        if in_tab == 'sections':
+            return cls.get_sections()
+        elif in_tab == 'student':
+            return cls.get_student()
+
+    @classmethod
+    def get_sections(cls, ):
+        template_values = {}
+        template_values['main_content'] = 'Course Section Content'
+
+        return template_values['main_content']
+
+    @classmethod
+    def get_student(cls):
+        template_values = {}
+        template_values['main_content'] = 'Student Dashboard Content'
+
+        return template_values['main_content']
+
+
+#Not needed as far as I know, at least, until we run into a scenario where we might need to define roles specific to
+# this module (can edit students maybe, something like that)
+# def permissions_callback(app_context):
+#     return [
+#             roles.Permission(ACCESS_ASSETS_PERMISSION, ACCESS_ASSETS_PERMISSION_DESCRIPTION),
+#             roles.Permission(ACCESS_SETTINGS_PERMISSION, ACCESS_SETTINGS_PERMISSION_DESCRIPTION),
+#             roles.Permission(ACCESS_ROLES_PERMISSION, ACCESS_ROLES_PERMISSION_DESCRIPTION),
+#             roles.Permission(ACCESS_ANALYTICS_PERMISSION, ACCESS_ANALYTICS_PERMISSION_DESCRIPTION),
+#             roles.Permission(ACCESS_SEARCH_PERMISSION, ACCESS_SEARCH_PERMISSION_DESCRIPTION),
+#             roles.Permission(ACCESS_PEERREVIEW_PERMISSION, ACCESS_PEERREVIEW_PERMISSION_DESCRIPTION),
+#             roles.Permission(ACCESS_SKILLMAP_PERMISSION, ACCESS_SKILLMAP_PERMISSION_DESCRIPTION),
+#         ]
+
+def notify_module_enabled():
+
+    dashboard.DashboardHandler.add_nav_mapping(
+        TeacherHandler.ACTION, 'Teacher')
+
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_ASSETS_PERMISSION, ACCESS_ASSETS_PERMISSION_DESCRIPTION)
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_SETTINGS_PERMISSION, ACCESS_SETTINGS_PERMISSION_DESCRIPTION)
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_ROLES_PERMISSION, ACCESS_ROLES_PERMISSION_DESCRIPTION)
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_ANALYTICS_PERMISSION, ACCESS_ANALYTICS_PERMISSION_DESCRIPTION)
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_SEARCH_PERMISSION, ACCESS_SEARCH_PERMISSION_DESCRIPTION)
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_PEERREVIEW_PERMISSION, ACCESS_PEERREVIEW_PERMISSION_DESCRIPTION)
+    dashboard.DashboardHandler.add_external_permission(
+        ACCESS_SKILLMAP_PERMISSION, ACCESS_SKILLMAP_PERMISSION_DESCRIPTION)
+
+    #register tabs
+    TeacherHandler.register_tabs()
+
+    #Don't need to register permissions here, dashboard module takes care of that
+    #roles.Roles.register_permissions(
+    #    custom_module, permissions_callback)
+
+    #here's where I would register my entities, IF I HAD ANY
+    #courses.ADDITIONAL_ENTITIES_FOR_COURSE_IMPORT.add(ResourceBundleEntity)
+    #courses.ADDITIONAL_ENTITIES_FOR_COURSE_IMPORT.add(I18nProgressEntity)
+
+    #register handlers (register is in BaseDashboardExtension)
+    TeacherHandler.register()
+
+    #hooks would go here, none needed for a basic module. yet....
+
+
+def register_module():
+    """Registers this module in the registry."""
+
+    global_routes = [
+        (os.path.join(RESOURCES_PATH, 'js', '.*'), tags.JQueryHandler),
+        (os.path.join(RESOURCES_PATH, '.*'), tags.ResourcesHandler)
+       ]
+    namespaced_routes = [
+        #(TranslationConsoleRestHandler.URL, TranslationConsoleRestHandler),
+        #(TranslationDeletionRestHandler.URL, TranslationDeletionRestHandler),
+        #(TranslationDownloadRestHandler.URL, TranslationDownloadRestHandler),
+        #(TranslationUploadRestHandler.URL, TranslationUploadRestHandler),
+        #(IsTranslatableRestHandler.URL, IsTranslatableRestHandler)
+        ]
+
+    global custom_module  # pylint: disable=global-statement
+    custom_module = custom_modules.Module(
+        'Teacher Dashboard Module',
+        'A module provide teacher workflow.',
+        global_routes, namespaced_routes,
+        notify_module_enabled=notify_module_enabled)
+
+    return custom_module
